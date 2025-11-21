15f8d9a9b7d73eff248f09c78b4df1463910cc3a
Ana R. Carvalho <anarcarvalho93@gmail.com>
2025-11-21 18:21:45 -0300

    commit 8

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
+  
   resources :posts
+  
+  # Estudantes
   get "estudantes/index"
-  get "home", to: "home#index", as: "home"
-  get "estudantes", to: "estudante#index", as: "estudantes"
-  get "about", to: "pages#about"
+  get "estudantes", to: "estudantes#index", as: "estudantes"
 
+  # Páginas gerais
+  get "home",  to: "home#index",  as: "home"
+  get "about", to: "pages#about", as: "about"
+
+  # Página inicial
   root "home#index"
 end
