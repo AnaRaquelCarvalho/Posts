2d52e7d977f4cf4202c75b6fdfde1aaecab43cb0
Ana R. Carvalho <anarcarvalho93@gmail.com>
2025-11-21 14:50:35 -0300

    Novo Commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_21_141013) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_21_171319) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -19,5 +19,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_21_141013) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "views"
   end
 end
