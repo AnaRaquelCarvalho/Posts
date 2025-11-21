f7d762baff36a17e93ba7bd09635ce4ab2c42118
Ana R. Carvalho <anarcarvalho93@gmail.com>
2025-11-21 11:20:26 -0300

    atual.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :posts
   get "estudantes/index"
   get "home", to: "home#index", as: "home"
   get "estudantes", to: "estudante#index", as: "estudantes"
